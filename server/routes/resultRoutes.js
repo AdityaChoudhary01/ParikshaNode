bac1b5abb1a9ea9d73a5243ea584bb42997fe106
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-09-17 15:39:04 +0530

    Update resultRoutes.js

--- server/routes/resultRoutes.js
+++ server/routes/resultRoutes.js
@@ -3,17 +3,17 @@ import {
   getResultById,
   getMyQuizHistory,
   getLeaderboard,
+  getQuizReport
 } from '../controllers/resultController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-
 router.route('/my-history').get(protect, getMyQuizHistory);
-
 
 router.route('/leaderboard/:quizId').get(getLeaderboard);
 
+router.route('/report/:quizId').get(protect, getQuizReport);
 
 router.route('/:id').get(protect, getResultById);
 
