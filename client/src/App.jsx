661317ebe12dab1dc0873f5798339ef21a4fa274
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-09-17 15:34:54 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,27 +8,28 @@ import Layout from './components/layout/Layout';
 import PrivateRoute from './components/routes/PrivateRoute';
 import AdminRoute from './components/routes/AdminRoute';
 
-// Core Pages
+// Pages
 import HomePage from '@/pages/HomePage';
 import LoginPage from '@/pages/LoginPage';
 import RegisterPage from '@/pages/RegisterPage';
 import QuizPage from '@/pages/QuizPage';
 import ResultsPage from '@/pages/ResultsPage';
-import HistoryPage from '@/pages/HistoryPage';
-import LeaderboardPage from '@/pages/LeaderboardPage';
+import QuizDetailsPage from '@/pages/QuizDetailsPage';
+import SharedQuizHandler from '@/pages/SharedQuizHandler';
 import NotFoundPage from '@/pages/NotFoundPage';
-import AboutPage from '@/pages/AboutPage';
-import DonatePage from '@/pages/DonatePage';
-import ContactPage from '@/pages/ContactPage';
-import ProfilePage from '@/pages/ProfilePage';
-import MyQuizzesPage from '@/pages/MyQuizzesPage';
-import QuizFormPage from '@/pages/QuizFormPage'; // Corrected path using alias
-
-// Admin Pages
-import AdminLayout from '@/pages/admin/AdminLayout';
-import AdminDashboardPage from '@/pages/admin/AdminDashboardPage';
-import AdminQuizListPage from '@/pages/admin/AdminQuizListPage';
-import AdminUserListPage from '@/pages/admin/AdminUserListPage';
+import MyQuizzesPage from './pages/MyQuizzesPage';
+import QuizReportPage from './pages/QuizReportPage';
+import HistoryPage from './pages/HistoryPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import DonatePage from './pages/DonatePage';
+import AdminLayout from './pages/admin/AdminLayout';
+import AdminDashboardPage from './pages/admin/AdminDashboardPage';
+import AdminQuizListPage from './pages/admin/AdminQuizListPage';
+import AdminUserListPage from './pages/admin/AdminUserListPage';
+import ProfilePage from './pages/ProfilePage';
+import QuizFormPage from './pages/QuizFormPage';
+import LeaderboardPage from './pages/LeaderboardPage';
 
 function App() {
   return (
@@ -38,23 +40,29 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
-          <Route path="leaderboard/:quizId" element={<LeaderboardPage />} />
           <Route path="about" element={<AboutPage />} />
-          <Route path="donate" element={<DonatePage />} />
           <Route path="contact" element={<ContactPage />} />
-          <Route path="quiz/:id" element={<QuizPage />} />
+          <Route path="donate" element={<DonatePage />} />
+          <Route path="leaderboard/:quizId" element={<LeaderboardPage />} />
           
-          {/* Protected User Routes */}
-          <Route path="" element={<PrivateRoute />}>
+          {/* Shared Quiz Redirect */}
+          <Route path="quiz/:id" element={<SharedQuizHandler />} />
+          {/* Quiz Details */}
+          <Route path="quiz/details/:id" element={<QuizDetailsPage />} />
+
+          {/* Protected Routes */}
+          <Route element={<PrivateRoute />}>
+            <Route path="quiz/:id/start" element={<QuizPage />} />
+            <Route path="results/:id" element={<ResultsPage />} />
+            <Route path="history" element={<HistoryPage />} />
             <Route path="profile" element={<ProfilePage />} />
             <Route path="my-quizzes" element={<MyQuizzesPage />} />
             <Route path="quiz/new" element={<QuizFormPage />} />
             <Route path="quiz/edit/:id" element={<QuizFormPage />} />
-            <Route path="results/:id" element={<ResultsPage />} />
-            <Route path="history" element={<HistoryPage />} />
+            <Route path="quiz/report/:quizId" element={<QuizReportPage />} />
           </Route>
 
-          {/* Protected Admin Routes */}
+          {/* Admin Routes */}
           <Route path="admin" element={<AdminRoute />}>
             <Route element={<AdminLayout />}>
               <Route index element={<AdminDashboardPage />} />
@@ -63,11 +71,17 @@ function App() {
             </Route>
           </Route>
 
-          {/* Not Found Route */}
+          {/* Not Found */}
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
-      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} theme="colored" />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        theme="colored"
+      />
     </>
   );
 }
